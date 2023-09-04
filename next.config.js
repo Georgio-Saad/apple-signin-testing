d60909fef27237bc67ea5183020dd71e818139c5
Georgio-Saad <129726976+Georgio-Saad@users.noreply.github.com>
2023-09-04 11:20:55 +0300

    adding apple app-file association

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/:file',
+        destination: '/api/.well-known/:file',
+        permanent: false,
+      },
+    ];
+  },
   images: { domains: ['lh3.googleusercontent.com'] },
 };
 
